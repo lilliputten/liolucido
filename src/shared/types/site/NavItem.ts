4051f2c18f96904506edd1ca94e37800caf13050
lilliputten <lilliputten@gmail.com>
2025-07-06 19:44:36 +0300

    Added `DashboardSidebar`.

--- src/shared/types/site/NavItem.ts
+++ src/shared/types/site/NavItem.ts
@@ -2,6 +2,7 @@ import { Icons } from '@/components/shared/icons';
 import { TRoutePath } from '@/i18n/routing';
 
 export type NavItem = {
+  // title: string;
   titleId: string; // Id for i18n
   href: TRoutePath;
   badge?: number;
@@ -13,3 +14,14 @@ export type NavItem = {
 };
 
 export type MainNavItem = NavItem;
+
+export type MarketingConfig = {
+  mainNav: MainNavItem[];
+};
+
+export type SidebarNavItem = {
+  title: string;
+  items: NavItem[];
+  authorizeOnly?: UserRole;
+  icon?: keyof typeof Icons;
+};
