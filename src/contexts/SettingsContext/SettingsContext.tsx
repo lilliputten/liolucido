4ed122878bf3aaa4014356465246af5206cacd2a
lilliputten <lilliputten@gmail.com>
2025-07-22 16:07:55 +0300

    Issue #7: Fixed settings form & context non-memoizing issue.

--- src/contexts/SettingsContext/SettingsContext.tsx
+++ src/contexts/SettingsContext/SettingsContext.tsx
@@ -23,22 +23,21 @@ export function SettingsContextProvider({ children }: SettingsContextProviderPro
   const user = useSessionUser();
   const userId = user?.id;
 
+  // Load settings
   React.useEffect(() => {
-    const isUser = !!userId;
-    // console.log('[SettingsContext] Start', isUser, userId);
-    if (!isUser) {
+    console.log('[SettingsContext] Load settings: Start', userId);
+    if (!userId) {
       // Restore settings from a local storage, if presented (be careful about sensitive data)
       const settingsJson = window.localStorage.getItem('settings');
       if (settingsJson) {
         try {
           const settingsRaw = JSON.parse(settingsJson);
           const settings: TSettings = settingsSchema.parse(settingsRaw);
-          console.log('[SettingsContext] Parsed local settings', {
+          console.log('[SettingsContext] Load settings: Parsed local settings', {
             settingsJson,
             settingsRaw,
             settings,
           });
-          // debugger;
           setSettings(settings);
         } catch (error) {
           // eslint-disable-next-line no-console
@@ -50,19 +49,64 @@ export function SettingsContextProvider({ children }: SettingsContextProviderPro
       }
     } else {
       // TODO: Get user data from server, using loading sempahor
-      console.log('[SettingsContext] Retrieve settings from server for user:', userId);
+      console.log('[SettingsContext] Load settings from server for user:', userId);
     }
     setInited(true);
-  }, [userId]);
+  }, [userId, setInited]);
+
+  const updateLocalSettings = React.useCallback((settings: TSettings) => {
+    try {
+      const settingsJson = JSON.stringify(settings);
+      console.log('[SettingsContext:updateLocalSettings] Updating local settings', {
+        settingsJson,
+        settings,
+      });
+      window.localStorage.setItem('settings', settingsJson);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('[SettingsContext:updateLocalSettings] Can not save local settings', {
+        error,
+      });
+      debugger; // eslint-disable-line no-debugger
+    }
+  }, []);
+
+  const saveSettings = React.useCallback(
+    (settings: TSettings) => {
+      console.log('[SettingsContext:saveSettings] Save settings: Start', inited, userId, settings);
+      /* // TODO: Implement server save handler
+       * const savePromise = userId ? updateSettings(settings) : Promise.resolve(settings);
+       */
+      // DEBUG
+      const savePromise = new Promise<TSettings>((resolve, _reject) => {
+        // setTimeout(reject, 1000, 'Demo error!');
+        setTimeout(resolve, 1000, settings);
+      });
+      return savePromise.then(() => {
+        updateLocalSettings(settings);
+        setSettings(settings);
+        return settings;
+      });
+    },
+    [inited, userId, updateLocalSettings],
+  );
+
+  // Save settings
+  React.useEffect(() => {
+    if (!inited) {
+      return;
+    }
+  }, [inited, userId, settings]);
 
   const settingsContext = React.useMemo(
     () => ({
       user,
       settings,
       setSettings,
+      saveSettings,
       inited,
     }),
-    [user, settings, inited],
+    [user, settings, saveSettings, inited],
   );
 
   return <SettingsContext.Provider value={settingsContext}>{children}</SettingsContext.Provider>;
